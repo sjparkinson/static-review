f8a3d3d2a4cd52b162e13f0133d0a71ec503236e
Woody Gilk <woody.gilk@gmail.com>
2015-10-27 09:49:54 -0500

    Allow global tools to be used for processes
    Sets the `$cwd` of `Process` commands based on whether or not the
    current working directory matches with the installation path.
    
    Fixes #27
    Refs #46

--- tests/unit/Review/AbstractReviewTest.php
+++ tests/unit/Review/AbstractReviewTest.php
@@ -26,4 +26,29 @@ class AbstractReviewTest extends TestCase
 
         $this->assertInstanceOf('Symfony\Component\Process\Process', $process);
     }
+
+    public function testGetProcessWorkingDirectory()
+    {
+        $review = Mockery::mock('StaticReview\Review\AbstractReview')->makePartial();
+
+        $process = $review->getProcess('whoami');
+
+        // By default, the working directory should be the current directory.
+        $this->assertSame(getcwd(), $process->getWorkingDirectory());
+
+        $cwd = getcwd();
+
+        // Move out of the current working directory, which should cause the
+        // process root directory to match the original root rather than the
+        // current directory. This is done to handle global installation.
+        chdir(sys_get_temp_dir());
+
+        $process = $review->getProcess('whoami');
+
+        $this->assertSame($cwd, $process->getWorkingDirectory());
+        $this->assertNotSame(getcwd(), $process->getWorkingDirectory());
+
+        // Restore original working directory.
+        chdir($cwd);
+    }
 }
